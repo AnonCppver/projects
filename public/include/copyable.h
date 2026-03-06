77ea50cb520cbc0c32bc572c3284c9865d1dacd9
AnonCppver <ljq002453@163.com>
2026-03-06 14:03:57 +0800

    test

--- public/include/copyable.h
+++ public/include/copyable.h
@@ -3,10 +3,6 @@
 
 namespace prj
 {
-
-/// A tag class emphasises the objects are copyable.
-/// The empty base class optimization applies.
-/// Any derived class of copyable should be a value type.
 class copyable
 {
  protected:
